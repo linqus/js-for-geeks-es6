7f44dd34f014cc824d61048ffe673d53f7aed561
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-11 15:27:08 +0200

    const

--- play.js
+++ play.js
@@ -1,14 +1,19 @@
-console.log(aGreatNumber);
-var aGreatNumber = 10;
+const aGreatNumber = 10;
+const aGreatObject = {
+    withGreatKeys: true
+};
+
+aGreatObject.withGreatKeys = false;
 
 if (true) {
 
-        let aGreatNumber = 42;
+    const aGreatNumber = 42;
 
 }
 
 setTimeout( ()=> {
     console.log(aGreatNumber);
+    console.log(aGreatObject);
 }, 1000)
 
 console.log('waiting...');
