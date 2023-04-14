4f3131a11f704c3f36c5bf26d798149546b76df5
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-14 20:14:37 +0200

    template strings

--- play.js
+++ play.js
@@ -1,14 +1,6 @@
-let printThreeThings = function(thing1, thing2, thing3) {
-    console.log(thing1,thing2, thing3);
+const favoriteFoot = 'gelato';
+const iLoveFood = 'The year is '+ (new Date).getFullYear()+' and my favourite food is '+favoriteFoot+'.';
 
-}
+const iLoveFood2 = `The year is ${(new Date).getFullYear()} and my favourite food is ${favoriteFoot}.`;
 
-let yummyThings = ['pizza', 'burrito', 'bigos', 'cheesburger'];
-let greatThings = ['swimming', 'sunsets', ...yummyThings, 'New Orleans'];
-
-let copyOfGreatThings = [...greatThings]; //true copy
-copyOfGreatThings.push('summer');
-//printThreeThings(3,'a',[4,3]);
-console.log(greatThings, copyOfGreatThings);
-
-//printThreeThings('lody',...yummyThings);
+console.log(iLoveFood2);
