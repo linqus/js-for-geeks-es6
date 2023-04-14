100042d74bcd566692084ec94b51574820e535b0
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-14 20:00:23 +0200

    spread operator

--- play.js
+++ play.js
@@ -1,23 +1,14 @@
-class AGreateClass {
-    constructor(greatNumber) {
-        this.greatNumber = greatNumber;
-    }
+let printThreeThings = function(thing1, thing2, thing3) {
+    console.log(thing1,thing2, thing3);
 
-    returnGreatThings() {
-        return this.greatNumber;
-    }
 }
 
-class AnotherGreatClass extends AGreateClass {
-    constructor(greatNumber, greatWord) {
-        super(greatNumber);
-        this.greatWord = greatWord;
-    }
-    returnGreatThings() {
-        let greatThing = super.returnGreatThings();
-        return [greatThing, this.greatWord];
-    }
-}
+let yummyThings = ['pizza', 'burrito', 'bigos', 'cheesburger'];
+let greatThings = ['swimming', 'sunsets', ...yummyThings, 'New Orleans'];
 
-const aGreatObject = new AnotherGreatClass(42, 'adventure')
-console.log(aGreatObject.returnGreatThings());
+let copyOfGreatThings = [...greatThings]; //true copy
+copyOfGreatThings.push('summer');
+//printThreeThings(3,'a',[4,3]);
+console.log(greatThings, copyOfGreatThings);
+
+//printThreeThings('lody',...yummyThings);
